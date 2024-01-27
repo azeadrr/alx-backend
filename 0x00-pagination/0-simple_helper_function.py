71c3090a8989e76aa38f4cd3db5f52ec19dea4e4
azeadrr <azeadn1@gmail.com>
2024-01-27 15:44:55 -0800

    mandatory tasks

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """function named index_range
+    two integer arguments page and page_size"""
+    return ((page - 1) * page_size, page * page_size)
