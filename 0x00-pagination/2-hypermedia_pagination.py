5df62cdfde0b40d9371045a06c619746a6d2b305
azeadrr <azeadn1@gmail.com>
2024-01-28 07:20:56 -0800

    mandatory tasks

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -14,18 +14,20 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
 
 class Server:
-    """class to paginate a db of popular baby names"""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
-                read = csv.reader(f)
-                dataset = [row for row in read]
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
             self.__dataset = dataset[1:]
 
         return self.__dataset
